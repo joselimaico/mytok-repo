0f7e7a1ac9fee51a511b2f390eeb6ad11419aaa3
limaico <francisco.limaico@kushkipagos.com>
2022-04-23 11:46:26 -0500

    changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <main>
+      tik tok clone 
+      </main>
+    </div>
+  )
+}
+
+export default App
